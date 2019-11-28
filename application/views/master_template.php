5ff3c8ce3277e544f8f732c45bc8205786bb3f6e
ashishkpaul <ashish.kpaul@gmail.com>
2019-11-28 19:16:27 +0530

    Update master_template.php

--- application/views/master_template.php
+++ application/views/master_template.php
@@ -1,7 +1,7 @@
 <!doctype html>
 <html>
 <head>
-<title><?php echo $page_title; ?> | Mobile Repair</title>
+<title><?php echo $page_title; ?> | Mobile Repair Management</title>
 <meta charset="utf-8">
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
 <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0">
